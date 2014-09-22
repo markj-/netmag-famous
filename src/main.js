bbc937c1f78feb7b23cdf672fd3d77fcf5133e03
Mark Jones <mark@thisismarkup.co.uk>
2014-09-22 21:24:10 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+define(function(require, exports, module) {
+    var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
+    var Modifier = require('famous/core/Modifier');
+    var StateModifier = require('famous/modifiers/StateModifier');
+    var Transform = require('famous/core/Transform');
+    var View = require('famous/core/View');
+    var Easing = require('famous/transitions/Easing');
+    var Transitionable = require('famous/transitions/Transitionable');
+    var WallTransition = require('famous/transitions/WallTransition');
+
+    // your code here
+});
